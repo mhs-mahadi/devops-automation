aeca87c1df38e0eaa44865e154bdc7c26514f2a6
Shohag <mahadishohagbd@gmail.com>
2023-12-01 00:18:58 +0600

    tag name added

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to javatechie from tester";
+		return "welcome to javatechie from k8s by Mahadi Hassan Shohag";
 	}
 
 	public static void main(String[] args) {
